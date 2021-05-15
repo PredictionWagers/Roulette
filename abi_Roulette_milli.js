5bd7107ef763233ffb252de470e3b460f8dc67b0
PredictionWagers <58184904+PredictionWagers@users.noreply.github.com>
2021-05-15 14:53:18 -0500

    Add files via upload

---
+++ abi_Roulette_milli.js
@@ -0,0 +1,208 @@
+var gobjAbi_Roulette_milli = [
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "HouseAddress",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "PlayerAddress",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "uint256",
+                "name": "TimeStamp",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "int256",
+                "name": "CreditOrDebit",
+                "type": "int256"
+            }
+        ],
+        "name": "evtReport",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "sUsername",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nMaxPayoutPerSpin",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nWeiDeposit",
+                "type": "uint256"
+            }
+        ],
+        "name": "zCreateHouse",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "zGetHouses",
+        "outputs": [
+            {
+                "internalType": "address[]",
+                "name": "asAddresses",
+                "type": "address[]"
+            },
+            {
+                "internalType": "bytes32[]",
+                "name": "asUsernames",
+                "type": "bytes32[]"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "anBalances",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "anMaxPayoutPerSpins",
+                "type": "uint256[]"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "zGetWinningNumber",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "nWinningNumber",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "zGetgb32Random",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "b32Random",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "zKill",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "b32Random",
+                "type": "bytes32"
+            }
+        ],
+        "name": "zSetgb32Random",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "nChipValue",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nHouseID",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bool[]",
+                "name": "gbaAllSpaces",
+                "type": "bool[]"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nWeiDeposit",
+                "type": "uint256"
+            }
+        ],
+        "name": "zSpinWheel",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "sUsername",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nMaxPayoutPerSpin",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nWithdrawal",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "nWeiDeposit",
+                "type": "uint256"
+            }
+        ],
+        "name": "zUpdateHouse",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
